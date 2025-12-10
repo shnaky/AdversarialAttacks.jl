ebb309c5e6c944cf34dfbf0d2ba0ee8f19a81dbd
shnaky <orestis.papandreou@gmail.com>
2025-12-10 01:51:51 +0100

    feat: export Interface.jl functions

--- src/AdversarialAttacks.jl
+++ src/AdversarialAttacks.jl
@@ -2,12 +2,16 @@ module AdversarialAttacks
 
 include("attacks/Attack.jl")
 include("models/Model.jl")
+include("Interface.jl")
 
 using .Attack
 using .Model
+using .Interface: run, benchmark
 
 export AbstractAttack, WhiteBoxAttack, BlackBoxAttack, name, hyperparameters, craft
 
 export AbstractModel, DifferentiableModel, NonDifferentiableModel, predict, loss, params
+
+export run, benchmark
 
 end
