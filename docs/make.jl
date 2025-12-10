0bccf26fe8f055c95fa957ed2253a1558feae05f
Ezra Cerpac <e.cerpac@icloud.com>
2025-12-10 18:16:39 +0100

    Moved getting started to README.md (automatically syncs with docs), and got started with getting started

--- docs/make.jl
+++ docs/make.jl
@@ -1,26 +1,36 @@
 using AdversarialAttacks
 using Documenter
 
-DocMeta.setdocmeta!(AdversarialAttacks, :DocTestSetup, :(using AdversarialAttacks); recursive=true)
+# Create index.md from README
+cp(joinpath(@__DIR__, "..", "README.md"), joinpath(@__DIR__, "src", "index.md"); force=true)
+
+DocMeta.setdocmeta!(
+  AdversarialAttacks,
+  :DocTestSetup,
+  :(using AdversarialAttacks);
+  recursive=true,
+)
 
 makedocs(;
-    modules=[AdversarialAttacks],
-    authors="FirstName LastName <orestis.papandreou@campus.tu-berlin.de>",
-    sitename="AdversarialAttacks.jl",
-    format=Documenter.HTML(;
-        canonical="https://shnaky.github.io/AdversarialAttacks.jl",
-        edit_link="main",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-        "Getting Started" => "getting_started.md",
-        "Attack Interface" => "attack_interface.md",
-        "Model Interface" => "model_interface.md",
+  modules=[AdversarialAttacks],
+  # TODO: change author info
+  authors="FirstName LastName <orestis.papandreou@campus.tu-berlin.de>",
+  sitename="AdversarialAttacks.jl",
+  format=Documenter.HTML(;
+    canonical="https://shnaky.github.io/AdversarialAttacks.jl",
+    edit_link="main",
+    assets=String[], # TODO: create icon
+  ),
+  pages=[
+    "Getting Started" => "index.md",
+    "Developer Documentation" => [
+      "Attacks" => "attack_interface.md",
+      "Model Interface" => "model_interface.md",
     ],
+  ],
 )
 
 deploydocs(;
-    repo="github.com/shnaky/AdversarialAttacks.jl",
-    devbranch="main",
+  repo="github.com/shnaky/AdversarialAttacks.jl",
+  devbranch="main",
 )
