add7cb973159193542bdb41b82812eae4871223e
m-buschmann <snep114@gmail.com>
2025-12-11 22:39:25 +0100

    renamed file to match planned structure

--- src/AdversarialAttacks.jl
+++ src/AdversarialAttacks.jl
@@ -3,7 +3,7 @@ module AdversarialAttacks
 include("attacks/Attack.jl")
 using .Attack: AbstractAttack, WhiteBoxAttack, BlackBoxAttack, craft
 
-include("Fgsm.jl")
+include("attacks/WhiteBox.jl")
 using .FastGradientSignMethod: FGSM
 
 export FGSM, craft, AbstractAttack, WhiteBoxAttack, BlackBoxAttack
