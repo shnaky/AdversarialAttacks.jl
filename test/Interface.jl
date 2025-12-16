4589d9d851e0c381b9a252378341ebebb370c4c8
shnaky <orestis.papandreou@gmail.com>
2025-12-16 02:10:42 +0100

    test: Interface multiple dispatch behaviour tests

--- test/Interface.jl
+++ test/Interface.jl
@@ -7,33 +7,58 @@ const Interface = AdversarialAttacks.Interface
 const Attack = AdversarialAttacks.Attack
 const Model = AdversarialAttacks.Model
 
-@testset "Interface.jl" begin
+@testset "Interface module" begin
+
     struct DummyAttack <: Attack.AbstractAttack end
     struct DummyModel <: Model.AbstractModel end
-    Attack.craft(sample, ::DummyModel, ::DummyAttack; kwargs...) = sample .+ 1.0
+    # dummy craft dispatch
+    Attack.craft(sample, ::Model.AbstractModel, ::Attack.AbstractAttack; kwargs...) = sample .+ 1.0
 
-    @testset "Fallback run" begin
+    @testset "Invalid inputs" begin
         struct NotAnAttack end
         struct NotAModel end
-        @test_throws MethodError Interface.run(NotAnAttack(), DummyModel(), [1, 2, 3])
-        @test_throws MethodError Interface.run(DummyAttack(), NotAModel(), [1, 2, 3])
-        @test_throws MethodError Interface.run(DummyAttack(), DummyModel(), "asdf")
+
+        @test_throws MethodError Interface.attack(NotAnAttack(), DummyModel(), [1, 2, 3])
+        @test_throws MethodError Interface.attack(DummyAttack(), NotAModel(), [1, 2, 3])
+        @test_throws MethodError Interface.attack(DummyAttack(), DummyModel(), "asdf")
     end
 
-    @testset "Single run" begin
-        result = Interface.run(DummyAttack(), DummyModel(), [1.0, 2.0])
-        @test result == [2.0, 3.0]
+   @testset "Sample tests" begin
+        sample = [1.0, 2.0]
+        @test Interface.attack(DummyAttack(), DummyModel(), sample) == [2.0, 3.0]
+
+        matrix = ones((2,2))
+        @test Interface.attack(DummyAttack(), DummyModel(), matrix) == [2.0 2.0; 2.0 2.0]
+
+        tensor = ones(2, 2, 3)
+        result = Interface.attack(DummyAttack(), DummyModel(), tensor)
+        @test result == fill(2.0, 2, 2, 3)
     end
 
-    @testset "Batch run" begin
-        samples = [[1.0, 2.0], [3.0, 4.0]]
-        result = Interface.run(DummyAttack(), DummyModel(), samples)
-        @test result == [[2.0, 3.0], [4.0, 5.0]]
+    @testset "WhiteBox / BlackBox dispatch" begin
+        WB = Attack.WhiteBoxAttack
+        BB = Attack.BlackBoxAttack
+        Diff = Model.DifferentiableModel
+        NonDiff = Model.NonDifferentiableModel
+
+        struct DummyWB <: WB end
+        struct DummyBB <: BB end
+        struct DummyDiffModel <: Diff end
+        struct DummyNonDiffModel <: NonDiff end
+
+        # minimal dummy craft methods to test Interface.attack dispatch behavior
+        Attack.craft(sample, ::Diff, ::WB; kwargs...) = sample .* 2
+        Attack.craft(sample, ::NonDiff, ::BB; kwargs...) = sample .* 3
+
+        @test Interface.attack(DummyWB(), DummyDiffModel(), [1.0, 2.0]) == [2.0, 4.0]
+        @test Interface.attack(DummyBB(), DummyNonDiffModel(), [1.0, 2.0]) == [3.0, 6.0]
+        # whitebox attack with non-differentiable model is not allowed
+        @test_throws ErrorException Interface.attack(DummyWB(), DummyNonDiffModel(), [1.0, 2.0])
     end
 
     @testset "Benchmark" begin
         dataset = [([1.0], 0), ([2.0], 1)]
-        function metric(adv_samples, labels)
+        function metric(model, adv_samples, labels)
             @test length(adv_samples) == length(labels)
             return length(adv_samples)
         end
