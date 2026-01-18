602f2caf87a2533d83d364fe3b5af57054a91b99
m-buschmann <snep114@gmail.com>
2026-01-18 14:18:51 +0100

    fix: adapted tests and README to avoid abstract types in struct fields

--- test/attacks/FgsmAttack.jl
+++ test/attacks/FgsmAttack.jl
@@ -8,13 +8,13 @@ using Flux: Chain, Dense
     # Test default constructor
     attack = FGSM()
     @test attack isa FGSM
-    @test attack.parameters == Dict{String,Any}()
+    @test attack.epsilon == 0.1
 
-    # Test constructor with parameters
-    params = Dict("epsilon" => 0.25)
-    attack_with_params = FGSM(params)
-    @test attack_with_params isa FGSM
-    @test hyperparameters(attack_with_params) == params
+    # Test constructor with keyword argument
+    attack_with_epsilon = FGSM(epsilon=0.25)
+    @test attack_with_epsilon isa FGSM
+    @test attack_with_epsilon.epsilon == 0.25
+    @test hyperparameters(attack_with_epsilon) == Dict("epsilon" => 0.25)
 
     # Test type hierarchy
     @test FGSM <: WhiteBoxAttack
@@ -27,7 +27,7 @@ using Flux: Chain, Dense
     sample_simple = (data=[1.0, 2.0, 3.0], label=[0.0, 1.0, 0.0])
     test_model = TestModel()
 
-    result_simple = craft(sample_simple, test_model, attack_with_params)
+    result_simple = craft(sample_simple, test_model, attack_with_epsilon)
     @test size(result_simple) == size(sample_simple.data)
     @test eltype(result_simple) == eltype(sample_simple.data)
 
@@ -35,7 +35,7 @@ using Flux: Chain, Dense
     sample = (data=Float32[1.0, 2.0, 3.0], label=Flux.onehot(1, 1:2))
     model = FluxModel(Chain(Dense(3 => 2)))
 
-    result = craft(sample, model, attack_with_params)
+    result = craft(sample, model, attack_with_epsilon)
     @test result isa Vector
     @test size(result) == size(sample.data)
     @test eltype(result) == eltype(sample.data)
