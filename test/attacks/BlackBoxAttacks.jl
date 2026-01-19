22c43fb1e8545a046262b41cb88fa01e01766a57
H <26712844+hhggu@users.noreply.github.com>
2026-01-19 13:21:08 +0100

    add seed for fixed Random on Unit test to make unit test stable (#77)
    Co-authored-by: Hailey <47661750+HaileyGu@users.noreply.github.com>

--- test/attacks/BlackBoxAttacks.jl
+++ test/attacks/BlackBoxAttacks.jl
@@ -1,11 +1,13 @@
 using Test
 using AdversarialAttacks
+using Random
 
 # Shared dummy model for black-box attack tests
 struct DummyBlackBoxModel <: AbstractModel end
 AdversarialAttacks.predict(::DummyBlackBoxModel, x) = x
 
 @testset "BasicRandomSearch Struct" begin
+    Random.seed!(1234)  # Make tests deterministic
 
     # Test default constructor
     attack = BasicRandomSearch()
@@ -24,6 +26,7 @@ AdversarialAttacks.predict(::DummyBlackBoxModel, x) = x
 
     # Test for FluxModel
     sample = (data=Float32[1.0, 2.0, 3.0, 7.0], label=Flux.onehot(1, 1:2))
+    Random.seed!(1234)
     model = FluxModel(Chain(Dense(4 => 2)))
     x_copy = copy(sample.data)
 
@@ -31,7 +34,7 @@ AdversarialAttacks.predict(::DummyBlackBoxModel, x) = x
     @test result isa Vector
     @test size(result) == size(sample.data)
     @test eltype(result) == eltype(sample.data)
-    @test x_copy == sample.data  
+    @test x_copy == sample.data
     @test result != sample.data #as we know epsilon is larger 0 here
     # TODO: Add tests for different models
 
