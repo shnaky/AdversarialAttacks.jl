9ae4948a052775604bfeb43d410775cac78f01d1
Ezra Cerpac <e.cerpac@icloud.com>
2026-01-23 18:17:49 +0100

    fix(docs): add `using Flux` to make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,6 @@
 using AdversarialAttacks
 using Documenter
+using Flux
 
 # Create index.md from README
 cp(joinpath(@__DIR__, "..", "README.md"), joinpath(@__DIR__, "src", "index.md"); force = true)
