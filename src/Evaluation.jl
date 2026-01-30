7b3617d27e2705b022d2a588a7026ae8c6cd6b60
hhggu <26712844+hhggu@users.noreply.github.com>
2026-01-30 20:38:54 +0100

    use dispatch

--- src/Evaluation.jl
+++ src/Evaluation.jl
@@ -410,41 +410,47 @@ Create a unified prediction function for evaluation only.
 This is NOT passed to attack() - only used for getting predictions.
 
 # Arguments
-
 - `model`: Either a Flux-style model or an MLJ Machine.
 
-
 # Returns
-
 - A function that takes input data and returns prediction probabilities as a vector.
 
-
 # Note
-
 This function handles different model types (Flux neural networks vs MLJ models)
 and input shapes (vectors vs matrices) to provide a consistent prediction interface.
 """
+function make_prediction_function(model::Machine)
+    return function (x_data)
+        if x_data isa AbstractVector
+            x_row = reshape(x_data, 1, :)
+        else
+            x_row = reshape(vec(x_data), 1, :)
+        end
+
+        X_tbl = table(x_row)
+        pred_dist = predict(model, X_tbl)[1]
+
+        class_levels = levels(pred_dist)
+        probs = [pdf(pred_dist, level) for level in class_levels]
+
+        return probs
+    end
+end
+
+"""
+    make_prediction_function(model)
+
+Flux model (Neural Network) version.
+
+# Arguments
+- `model`: Flux-compatible neural network model.
+
+# Returns
+- A function that takes input data and returns prediction vector.
+"""
 function make_prediction_function(model)
-    if model isa Machine
-        return function (x_data)
-            if x_data isa AbstractVector
-                x_row = reshape(x_data, 1, :)
-            else
-                x_row = reshape(vec(x_data), 1, :)
-            end
-
-            X_tbl = table(x_row)
-            pred_dist = predict(model, X_tbl)[1]
-
-            class_levels = levels(pred_dist)
-            probs = [pdf(pred_dist, level) for level in class_levels]
-
-            return probs
-        end
-    else
-        return function (x)
-            output = model(x)
-            return vec(output)
-        end
+    return function (x)
+        output = model(x)
+        return vec(output)
     end
 end
